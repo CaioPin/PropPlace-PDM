a9aff2c6b8f490ebbcc5d86ff85ca99ccada955e
CaioPin <college13579@gmail.com>
2024-08-24 15:32:15 -0300

    adiciona componente mapa

--- src/utils/elementosEstilizaveis.ts
+++ src/utils/elementosEstilizaveis.ts
@@ -1,10 +1,12 @@
 import { Image, ScrollView, Text, TextInput, View } from "react-native";
 import { styled } from "nativewind";
+import MapView from "react-native-maps";
 
 const StyledImage = styled(Image);
 const StyledScrollView = styled(ScrollView);
 const StyledText = styled(Text);
 const StyledTextInput = styled(TextInput);
 const StyledView = styled(View);
+const StyledMapView = styled(MapView);
 
-export { StyledImage, StyledScrollView, StyledText, StyledTextInput, StyledView };
+export { StyledImage, StyledScrollView, StyledText, StyledTextInput, StyledView, StyledMapView };
