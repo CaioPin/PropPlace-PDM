c54ff32c2734b23f49966c1cd098933dd73f88f7
CaioPin <college13579@gmail.com>
2024-08-25 00:47:35 -0300

    adiciona componente calendario

--- src/utils/elementosEstilizaveis.ts
+++ src/utils/elementosEstilizaveis.ts
@@ -1,10 +1,12 @@
 import { Image, ScrollView, Text, TextInput, View } from "react-native";
 import { styled } from "nativewind";
+import { Calendar } from "react-native-calendars";
 
+const StyledCalendar = styled(Calendar);
 const StyledImage = styled(Image);
 const StyledScrollView = styled(ScrollView);
 const StyledText = styled(Text);
 const StyledTextInput = styled(TextInput);
 const StyledView = styled(View);
 
-export { StyledImage, StyledScrollView, StyledText, StyledTextInput, StyledView };
+export { StyledCalendar, StyledImage, StyledScrollView, StyledText, StyledTextInput, StyledView };
