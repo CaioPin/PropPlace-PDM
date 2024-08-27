a4f874d4ec0d56c21fc0daaa884362fd11583d2b
Marianna <Marianna.lopes@academico.ifpb.edu.br>
2024-08-26 22:25:04 -0300

    Adiciona ícones

--- src/assets/icons/iconesLib.tsx
+++ src/assets/icons/iconesLib.tsx
@@ -4,6 +4,8 @@ import Fontisto from '@expo/vector-icons/Fontisto';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
 import { cores } from '../../constants/cores';
 
@@ -12,7 +14,10 @@ export const iconesLib = {
     cadeado: <AntDesign name="lock1" size={20} color={cores.secundaria} />,
     email: <Fontisto name="email" size={20} color={cores.secundaria} />,
     lixeira: <Feather name="trash-2" size={14} color={cores.branco} />,
-    lupa: <AntDesign name="search1" size={20} color={cores.secundaria} />,
+    lupa: <AntDesign name="search1" size={24} color={cores.secundaria} />,
     pessoa: <Ionicons name="person" size={20} color={cores.secundaria} />,
-    telefone: <MaterialCommunityIcons name="cellphone" size={20} color={cores.secundaria} />
+    telefone: <MaterialCommunityIcons name="cellphone" size={20} color={cores.secundaria} />,
+    home: <FontAwesome6 name="house" size={24} color={cores.secundaria} />,
+    mapa: <Feather name="map-pin" size={24} color={cores.secundaria} />,
+    user: <FontAwesome5 name="user-circle" size={24} color={cores.secundaria} />
 }
