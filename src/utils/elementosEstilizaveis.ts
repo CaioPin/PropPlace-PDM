c7f7af795a9a5a2ba15cf9d2ddd750e6050a98df
Marianna <Marianna.lopes@academico.ifpb.edu.br>
2024-08-26 22:26:10 -0300

    Adiciona elemento Button

--- src/utils/elementosEstilizaveis.ts
+++ src/utils/elementosEstilizaveis.ts
@@ -1,10 +1,12 @@
 import { Image, ScrollView, Text, TextInput, View } from "react-native";
 import { styled } from "nativewind";
+import { Button } from "@rneui/base";
 
 const StyledImage = styled(Image);
 const StyledScrollView = styled(ScrollView);
 const StyledText = styled(Text);
 const StyledTextInput = styled(TextInput);
 const StyledView = styled(View);
+const StyledButton = styled(Button);
 
-export { StyledImage, StyledScrollView, StyledText, StyledTextInput, StyledView };
+export { StyledImage, StyledScrollView, StyledText, StyledTextInput, StyledView, StyledButton };
