021c2c4e73b8fabcf32c4cff1775909557632edf
Marianna <Marianna.lopes@academico.ifpb.edu.br>
2024-08-27 18:24:01 -0300

    Adiciona borda no topo do componente Menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,34 +1,34 @@
 import { iconesLib } from "../assets/icons/iconesLib";
 import { ConstrutorEstiloConstante } from "../utils/ConstrutorEstiloConstante";
-import { StyledButton, StyledText, StyledView } from "../utils/elementosEstilizaveis";
+import { StyledButton, StyledView } from "../utils/elementosEstilizaveis";
 
 function Menu(){
  
     return(
-        <StyledView className="flex flex-row justify-between w-full">
+        <StyledView className="flex flex-row justify-between w-full px-2 border-t-2 border-[#554348]">
 
-        <StyledButton buttonStyle={{width: 80}} 
+        <StyledButton buttonStyle={{flexGrow: 1}} 
         icon={iconesLib.home} 
         title={"Home"} 
         iconPosition="top" 
         titleStyle={estilo.texto} 
         type="clear"/>
 
-        <StyledButton buttonStyle={{width: 80}} 
+        <StyledButton buttonStyle={{flexGrow: 1}} 
         icon={iconesLib.lupa2} 
         title={"Pesquisar"} 
         iconPosition="top" 
         titleStyle={estilo.texto} 
         type="clear" />
 
-        <StyledButton buttonStyle={{width: 80}} 
+        <StyledButton buttonStyle={{flexGrow: 1}} 
         icon={iconesLib.mapa} 
         title={"Mapa"} 
         iconPosition="top" 
         titleStyle={estilo.texto} 
         type="clear"/>
 
-        <StyledButton buttonStyle={{width: 80}} 
+        <StyledButton buttonStyle={{flexGrow: 1}} 
         icon={iconesLib.user} 
         title={"Perfil"} 
         iconPosition="top" 
