3478a1cc311fe3a36a78d43668f9059abcf0fefc
Marianna <Marianna.lopes@academico.ifpb.edu.br>
2024-08-30 19:28:16 -0300

    Adiciona atributo de titulo na checkbox

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,35 +1,37 @@
 import { iconesLib } from "@/assets/icons/iconesLib"
 import { CheckBox } from "@rneui/base"
-import { View } from "react-native"
+import { View, Text } from "react-native"
 import { cores } from "@/constants/cores";
 import { useState } from "react";
 import { ConstrutorEstiloConstante } from "@/utils/ConstrutorEstiloConstante";
 
-enum CheckboxPossiveis{
-    tiposImovel = "tiposImovel",
-    filtroPessoa = "filtroPessoa",
-    tempoContrato = "tempoContrato",
-    filtroImovel = "filtroImovel",
+enum CheckboxTitulo{
+    tiposImovel = "Filtrar imóveis por tipo:",
+    filtroPessoa = "Filtrar usuários:",
+    tempoContrato = "Tempo de contrato",
+    filtroImovel = "Filtrar imóveis:",
 }
 
-const CheckboxOpcoes: { [key in CheckboxPossiveis]: string[] } = {
-    [CheckboxPossiveis.tiposImovel]: ["Apartamento", "Casa", "Kitnet", "República"],
-    [CheckboxPossiveis.filtroPessoa]: ["Inquilino", "Proprietário"],
-    [CheckboxPossiveis.tempoContrato]: ["1 ano", "Personalizado"],
-    [CheckboxPossiveis.filtroImovel]: ["Livre", "Alugado"]
+const CheckboxOpcoes: { [key in CheckboxTitulo]: string[] } = {
+    [CheckboxTitulo.tiposImovel]: ["Apartamento", "Casa", "Kitnet", "República"],
+    [CheckboxTitulo.filtroPessoa]: ["Inquilino", "Proprietário"],
+    [CheckboxTitulo.tempoContrato]: ["1 ano", "Personalizado"],
+    [CheckboxTitulo.filtroImovel]: ["Livre", "Alugado"]
 };
 
 interface CheckboxPropriedades{
     opcoes: string[],
-    separador: boolean
+    separador: boolean,
+    titulo: CheckboxTitulo,
 }
 
-function Checkbox({opcoes, separador}: CheckboxPropriedades){
+function Checkbox({opcoes, separador, titulo}: CheckboxPropriedades){
     const [check, setCheck] = useState<number | null>(0);
     const quantidade = opcoes.length;
 
         return(
             <View>
+                <Text className="font-black mb-2 pl-6" style={estilo.tituloEstilo}>{titulo}</Text>
                 {opcoes.map((opcaoNome, index) => (
                     <CheckBox
                     containerStyle={index === quantidade -1 || 
@@ -53,6 +55,7 @@ const estilo = {
     containerComLinha: {paddingTop: 0, minWidth:320, 
         borderBottomWidth: 2, borderColor: cores.secundaria},
     textoEstilo: ConstrutorEstiloConstante.construtor().fonteGG().corSecundaria().construir(),
+    tituloEstilo: ConstrutorEstiloConstante.construtor().fonteXG().corSecundaria().construir(),
 }
 
-export {Checkbox, CheckboxOpcoes};
+export {Checkbox, CheckboxOpcoes, CheckboxTitulo};
