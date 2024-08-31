2dfc1f2d308147d2d1c2b19f1875cbb98ebd959a
Verissimo Terceiro <terceiroverissimo@gmail.com>
2024-08-31 19:39:22 -0300

    ajustes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,5 @@
-import { Text, View } from "react-native"
+import { View } from "react-native";
 
 export default function App() {
-  return (
-    <View className="flex-1 justify-center items-center px-5">
-      <Text className="font-regular">INDEX</Text>
-      <Text className="font-medium">testa com font-medium</Text>
-      <Text>testa sem font</Text>
-      <Text className="font-regular">testa font-regular</Text>
-
-      <Text className="font-semibold">testa font-semibold</Text>
-    </View>
-  )
+  return <View className="flex-1 justify-center items-center px-5"></View>;
 }
