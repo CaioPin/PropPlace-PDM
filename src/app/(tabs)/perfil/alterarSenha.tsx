75b9c956a9d470c1b49573eeb4f982cb419db031
Verissimo Terceiro <terceiroverissimo@gmail.com>
2024-09-17 04:25:05 -0300

    cria tela alterarSenha

---
+++ src/app/(tabs)/perfil/alterarSenha.tsx
@@ -0,0 +1,155 @@
+import { router } from "expo-router";
+import { Text, View } from "react-native";
+import { useState } from "react";
+
+import { useAuthContext } from "@/hooks/useAuthContext";
+
+import { Campo, CampoIcones } from "@/components/Campo";
+import { Botao } from "@/components/Botao";
+
+import { validacoesUsuario } from "@/utils/validacoes";
+import { api } from "@/api";
+import { pegaStatusDeErro } from "@/utils/pegaStatusDeErro";
+import { Modal } from "@/components/Modal";
+
+export default function AlterarSenha() {
+  const estaAberto = router.canGoBack();
+  const [senhaAtual, defineSenhaAtual] = useState("");
+  const [novaSenha, defineNovaSenha] = useState("");
+  const [novaSenhaRepete, defineNovaSenhaRepete] = useState("");
+  const [mensagemModal, defineMensagemModal] = useState("");
+  const { userId, isLoading, deslogar } = useAuthContext();
+
+  async function realizaAlteracao() {
+    const validacaoNovaSenha = validacoesUsuario({
+      senha: novaSenha,
+    });
+
+    if (validacaoNovaSenha.length !== 0) {
+      defineMensagemModal(
+        "Certifique-se de que a nova senha tem no mínimo 8 caracteres e nenhum espaço vazio."
+      );
+      return;
+    }
+
+    if (novaSenha !== novaSenhaRepete) {
+      defineMensagemModal(
+        "Certifique-se de que a nova senha foi repetida corretamente."
+      );
+      return;
+    }
+
+    if (!senhaAtual) {
+      defineMensagemModal("Certifique-se de preencher os campos corretamente.");
+      return;
+    }
+
+    const resultadoStatus = await patchSenha(userId!, senhaAtual, novaSenha);
+
+    switch (resultadoStatus) {
+      case 500:
+        defineMensagemModal("Erro de conexão com o servidor.");
+        break;
+      case 400:
+        defineMensagemModal("Campos preenchidos incorretamente.");
+        break;
+      case 401:
+        defineMensagemModal("Senha atual incorreta.");
+        break;
+      case 200:
+        defineMensagemModal(
+          "Senha atualizada com sucesso! Faça login novamente."
+        );
+        break;
+      default:
+        break;
+    }
+  }
+
+  async function patchSenha(
+    userId: string,
+    senhaAtual: string,
+    novaSenha: string
+  ) {
+    try {
+      const resposta = await api.patch(`/users/${userId}/password`, {
+        antigaSenha: senhaAtual,
+        senha: novaSenha,
+      });
+
+      return resposta.status;
+    } catch (erro) {
+      const resposta = pegaStatusDeErro(erro);
+      return resposta?.status;
+    }
+  }
+
+  return (
+    <View className="flex-1 justify-center bg-paleta-fundo/60">
+      <View className="bg-paleta-fundo p-8 mx-2 rounded-xl border-2 border-paleta-secundaria/60 gap-6">
+        <Text className="text-paleta-secundaria font-extrabold text-xg text-left pb-2">
+          Altere sua senha
+        </Text>
+
+        <Campo
+          ativo
+          autoCapitalize="none"
+          autoFocus
+          secureTextEntry
+          value={senhaAtual}
+          onChangeText={defineSenhaAtual}
+          icone={CampoIcones.CADEADO}
+          placeholder="Senha atual"
+          aoMudar={() => {}}
+        />
+
+        <Campo
+          ativo
+          autoCapitalize="none"
+          secureTextEntry
+          value={novaSenha}
+          onChangeText={defineNovaSenha}
+          icone={CampoIcones.CADEADO}
+          placeholder="Nova senha"
+          aoMudar={() => {}}
+        />
+
+        <Campo
+          ativo
+          autoCapitalize="none"
+          secureTextEntry
+          value={novaSenhaRepete}
+          onChangeText={defineNovaSenhaRepete}
+          icone={CampoIcones.CADEADO}
+          placeholder="Repita a nova senha"
+          aoMudar={() => {}}
+        />
+
+        {!isLoading && (
+          <View className="flex-row items-center justify-between mt-4 px-2">
+            {estaAberto && (
+              <Text
+                onPress={() => router.back()}
+                className="text-regular text-blue-700">
+                Voltar
+              </Text>
+            )}
+            <Botao variante="enviar" onPress={realizaAlteracao}>
+              <Botao.Titulo>Enviar</Botao.Titulo>
+            </Botao>
+          </View>
+        )}
+
+        <Modal
+          titulo={mensagemModal}
+          visible={!!mensagemModal}
+          onClose={() => {
+            defineMensagemModal("");
+            if (mensagemModal.includes("sucesso")) {
+              deslogar();
+            }
+          }}/>
+      </View>
+    </View>
+  );
+}
