06730a38b04d961dbbdeb675f2102408ac92159c
Verissimo Terceiro <terceiroverissimo@gmail.com>
2024-09-17 09:08:20 -0300

    configura .env

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // url local do pc de voces. a mesma que o expo tá rodando,
-  // mas com a porta da api
-  baseURL: "http://192.168.0.194:3000",
+  baseURL: process.env.EXPO_PUBLIC_API_URL,
 });
