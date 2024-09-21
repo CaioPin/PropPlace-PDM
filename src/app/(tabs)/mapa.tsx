341c7842601038bf963d6af70e6911a2b8207556
Verissimo Terceiro <terceiroverissimo@gmail.com>
2024-09-20 23:38:05 -0300

    implementa geocoding api com google maps

--- src/app/(tabs)/mapa.tsx
+++ src/app/(tabs)/mapa.tsx
@@ -1,55 +1,86 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { GEOCODING_TOKEN } from "@env";
 import { cores } from "@/constants/cores";
 import { Campo, CampoIcones } from "@/components/Campo";
 import { Mapa } from "@/components/Mapa";
 import { Modal } from "@/components/Modal";
+import axios from "axios";
 
 let atraso = setTimeout(() => {}, 1);
 
-interface Coordenadas {
-    latitude: number,
-    longitude: number
+interface GeocodeResponse {
+  results: {
+    geometry: {
+      location: {
+        lat: number;
+        lng: number;
+      };
+    };
+  }[];
+  status: string;
 }
-
 export default function TelaMapa() {
-    const [coordenadas, definirCoordenadas] = useState<Coordenadas>();
-    const [mensagemErro, definirMensagemErro] = useState("Carai2");
-    
-    async function buscar(textoBusca:string) {
-        if (!textoBusca) return;
+  const [coordenadas, definirCoordenadas] = useState<{
+    latitude: number;
+    longitude: number;
+  }>();
+  const [mensagemErro, definirMensagemErro] = useState("");
+  const [textoBusca, definirTextoBusca] = useState("");
 
-        const geocodingApiUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
-        const url = `${geocodingApiUrl}${textoBusca}.json?access_token=${GEOCODING_TOKEN}`;
-        console.log(url);
-        
-        try {
-            // TOOD: substituir fetch por axios
-            const resposta = await fetch(url).then(resposta => resposta.json());
-            const coordenadasBusca = resposta?.features[0]?.center;
-            definirCoordenadas({latitude: coordenadasBusca[1], longitude: coordenadasBusca[0]});
-        } catch (erro) {
-            definirMensagemErro(`Erro ao buscar por "${textoBusca}". Por favor, tente novamente em instantes...`);
-        }
+  async function buscar(textoBusca: string) {
+    if (!textoBusca) return;
+
+    const geocodingApiUrl = `https://maps.googleapis.com/maps/api/geocode/json?language=pt-BR&key=${process.env.EXPO_PUBLIC_MAPS_API_KEY}&address=`;
+
+    try {
+      const resposta = await axios.get<GeocodeResponse>(
+        geocodingApiUrl + textoBusca
+      );
+      const coordsResposta = resposta.data.results[0]?.geometry.location;
+      const { lat: latitude, lng: longitude } = coordsResposta;
+      definirCoordenadas({ latitude, longitude });
+    } catch (erro) {
+      definirMensagemErro(
+        `Erro ao buscar por "${textoBusca}". Por favor, tente novamente em instantes...`
+      );
     }
+  }
 
-    function aoMudar(textoBusca:string) {
-        clearTimeout(atraso);
-        atraso = setTimeout(() => buscar(textoBusca), 1000);
-    }
+  function aoMudar(textoBusca: string) {
+    clearTimeout(atraso);
+    atraso = setTimeout(() => buscar(textoBusca), 1000);
+  }
 
-    return (
-        <SafeAreaView style={{ backgroundColor: cores.fundo, flexGrow: 1 }}>
-            <View className="flex gap-y-8 relative h-full px-8 pb-8 overflow-hidden">
-                <Campo texto="Pesquisar..." icone={CampoIcones.LUPA} valorInicial={""} aoMudar={aoMudar} ativo />
+  useEffect(() => {
+    aoMudar(textoBusca);
+  }, [textoBusca]);
 
-                <Mapa centro={coordenadas} realizarRequisicoes />
-                <View className="absolute left-8 bottom-0 bg-paleta-fundo w-full h-8"></View>
+  return (
+    <SafeAreaView style={{ backgroundColor: cores.fundo, flexGrow: 1 }}>
+      <View className="flex gap-y-8 relative h-full px-8 pb-8 overflow-hidden">
+        <Campo
+          ativo
+          value={textoBusca}
+          onChangeText={definirTextoBusca}
+          icone={CampoIcones.LUPA}
+          placeholder="Pesquisar..."
+          aoMudar={() => {}}
+        />
 
-                <Modal titulo={mensagemErro} visible={!!mensagemErro} onClose={() => definirMensagemErro("")} />
-            </View>
-        </SafeAreaView>
-    );
+        {coordenadas ? (
+          <Mapa centro={coordenadas} realizarRequisicoes />
+        ) : (
+          <Mapa />
+        )}
+        <View className="absolute left-8 bottom-0 bg-paleta-fundo w-full h-8"></View>
+
+        <Modal
+          titulo={mensagemErro}
+          visible={!!mensagemErro}
+          onClose={() => definirMensagemErro("")}
+        />
+      </View>
+    </SafeAreaView>
+  );
 }
