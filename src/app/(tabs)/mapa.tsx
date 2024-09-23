77c92f3cb0cb3cff25394117ce3d5bec579411ac
Marianna <Marianna.lopes@academico.ifpb.edu.br>
2024-09-23 18:47:55 -0300

    Troca nome

--- src/app/(tabs)/mapa.tsx
+++ src/app/(tabs)/mapa.tsx
@@ -1,6 +1,6 @@
 import { View, Text } from "react-native";
 
-export default function App() {
+export default function Mapa() {
   return <View className="flex-1 justify-center items-center px-5 bg-paleta-fundo">
           <Text>Map</Text>
   </View>;
